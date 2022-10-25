70b2f04ce45403003f9f3dac83d3a5040a1b5307
Stefano Dongowski <stefano@unc.edu>
2022-10-25 15:52:17 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Stefano Dongowski on 10/25/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
