544d8eff9a8cc790c94e9130605aff780a70599d
stefanodongowski <stefano@unc.edu>
2022-10-25 21:11:24 -0400

    Added functionality for both storing total value from user input and displaying final total value

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numPeople = 2
+    @State private var tipPercent = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            Section {
+                Text(checkAmount, format:
+                        .currency(code: Locale.current.currencyCode ?? "USD"))
+            }
         }
-        .padding()
     }
 }
 
